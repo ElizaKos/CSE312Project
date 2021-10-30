023986d385ff823f5990725e8a1596936879c57f
ElizaKoster <elizakos@buffalo.edu>
2021-10-30 16:01:16 -0400

    current dockerfile (not working).

--- cse312project/settings.py
+++ cse312project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #'CSE312Project'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'cse312project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cse312projectdb',
+        'USER': 'cse312projectuser',
+        'PASSWORD': 'ourpassword',
+        'HOST': '0.0.0.0',
+        'PORT': 5432,
     }
-}
+   }
 
 
 # Password validation
--- dockerfile
+++ dockerfile
@@ -4,5 +4,7 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
-
+ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1 /wait 
+RUN chmod +x /wait
+CMD /wait && python manage.py runserver 
 #https://docs.docker.com/language/python/build-images/
